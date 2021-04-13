2dd572bc38bca89b3d47f0cdbff2e77b7f34ebe4
MarioLion <66450569+MarioLion@users.noreply.github.com>
2021-04-13 13:27:49 -0500

    avence1

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
+<?php
+include("./controladores/conexion.php");
+session_start();
+if(isset($_SESSION['id_usuario'])){
+	header("Location:admin.php");
+}
+//insertar
+
+
+
+if(isset($_POST["registrar"])){
+	$nombre =mysqli_real_escape_string($conexion,$_POST['nombre']);
+	$correo =mysqli_real_escape_string($conexion,$_POST['correo']);
+	$usuario =mysqli_real_escape_string($conexion,$_POST['user']);
+	$password =mysqli_real_escape_string($conexion,$_POST['pass']);
+
+    $password_encriptada = sha1($password); 
+    $sqluser = "SELECT idusuario FROM usuarios WHERE usuario ='$usuario'";
+    $resultadouser= $conexion->query($sqluser);
+    $filas = $resultadouser->num_rows;
+    if ($filas > 0){
+    	echo"<script>
+    	alert('El usuario ya existe');
+    	window.location='index.php';
+    	</script>";
+
+    }else{
+    	//insertar informacion del usuario
+    	$sqlusuario="INSERT INTO usuarios(Nombre,Correo,Usuario,Password)
+    	VALUES ('$nombre','$correo','$usuario','$password_encriptada')";
+    	$resultadousuario =$conexion->query($sqlusuario);
+    	if($resultadousuario > 0){
+    		echo"<script>
+    		alert('Registro exito');
+    		window.location = 'index.php';
+    		</script>";
+    	}else{
+    		echo"<script>
+    		alert('Error al registrarse');
+    		window.location = 'index.php';
+    		</script>";
+    	}
+                   
+    }
+
+
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,13 +68,72 @@
 <body>
     
 
+<div class="container">
+<div class="row">
+    <div class="col-2"></div>
+        <div class="col-8">
+    <!--insertar -->
+    <form class="text-center border border-light p-5" action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST" >
+
+    <h1 class="h1 mb-4">Capturar</h1>
+
+    
+
+    <!-- Fecha-->
+    <p>Fecha</p>
+    <input type="date" name="fecha" class="form-control mb-4" placeholder="Fecha">
+<!-- Medicos-->
+    <p>Seleccione un medico del siguiente menú:</p>
+    <p>Médicos:
+      <select class="form-control mb-4">
+        <option value="0">Seleccione:</option>
+        <?php
+          $query = $conexion -> query ("SELECT * FROM medicos");
+          while ($valores = mysqli_fetch_array($query)) {
+            echo '<option value="'.$valores['id_medico'].'">'.$valores['nombre'].'</option>';
+          }
+        ?>
+      </select>
+<!-- pacientes -->
+<p>Seleccione un paciente del siguiente menú:</p>
+    <p>Pacientes:
+      <select class="form-control mb-4">
+        <option value="0">Seleccione:</option>
+        <?php
+          $query = $conexion -> query ("SELECT * FROM pacientes");
+          while ($valores = mysqli_fetch_array($query)) {
+            echo '<option value="'.$valores['id_paciente'].'">'.$valores['nombre'].'</option>';
+          }
+        ?>
+      </select>
+
+      <!-- Diagnostico -->
+<p>Seleccione un diagnostico del siguiente menú:</p>
+    <p>Diagnostico:
+      <select class="form-control mb-4">
+        <option value="0">Seleccione:</option>
+        <?php
+          $query = $conexion -> query ("SELECT * FROM diagnosticos");
+          while ($valores = mysqli_fetch_array($query)) {
+            echo '<option value="'.$valores['id'].'">'.$valores['descripcion'].'</option>';
+          }
+        ?>
+      </select>
+
+   
+    
+
+    
+    <button class="btn btn-info my-4 btn-block" type="submit">Guardar</button>
+    
+    </form>
 
 
+        </div>
+    <div class="col-2"></div>
+</div>    
 
-
-
-
-
+</div>
 
 
 
